824ae8952563a470fa59a447fd2b1f7e7c3fe629
borislemke <dimaslemke@gmail.com>
2018-08-22 19:42:07 +0700

    remove dist from git

--- dist/app.d.ts
+++
@@ -1,9 +0,0 @@
-export interface BootstrapConfig {
-    port: number | string;
-    turboMode?: boolean;
-    server?: any;
-}
-export declare class uApp {
-    static TURBO_MODE: boolean;
-    static bootstrap(app: any, config?: BootstrapConfig): Promise<any>;
-}
